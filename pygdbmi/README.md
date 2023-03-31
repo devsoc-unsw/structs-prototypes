dc87bff7433115e3dafd0287f1cf22b55c2c72d2
jedwed <83894732+jedwed@users.noreply.github.com>
2023-03-31 21:51:43 +1100

    Use argparse, user can now provide optional breakpoints

--- pygdbmi/README.md
+++ pygdbmi/README.md
@@ -1,13 +1,16 @@
 # pygdbmi
 
 A python library which gives programmatic control over GDB.
-- `test.py` is a program that runs GDB on an C executable prints the stack arguments and locals for each line of code executed
+- `main.py` is a program that runs GDB on an C executable prints the stack arguments and locals of the frame for each line of code executed
+
+## Usage
+- `python3 main.py code [breakpoints ...]`
 
 ## Setup
 ### Prerequisites
 - Python v3.8.10 or later
 - gcc & gdb to compile your code
-- The above should all be part of a docker container at some point
+- The above should all be in a docker container at some point
 
 ### Instructions
 1. Clone the repository and `cd` in this directory (`pygdbmi`)
--- pygdbmi/main.py
+++ pygdbmi/main.py
@@ -1,5 +1,6 @@
-import subprocess
 import sys
+import subprocess
+import argparse
 from pygdbmi.gdbcontroller import GdbController
 
 # COMMAND_TIMEOUT_SEC = 0.1
@@ -9,20 +10,33 @@ def main():
     """
     Main function
     """
-    code_file = sys.argv[1]
-    compile_code(code_file)
-    debug_code()
+    args = parse_args()
+    compile_code(args.code)
+    debug_code(args.breakpoints)
 
 
-def compile_code(code_file):
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Runs GDB on C code and prints stack locals and frames"
+    )
+    parser.add_argument("code", help="the filename of the C code")
+    parser.add_argument(
+        "breakpoints",
+        nargs="*",
+        help="the breakpoints (lines or functions) to be placed for debugging",
+    )
+    return parser.parse_args()
+
+
+def compile_code(code):
     """
     Compiles code_file into executable
     Note: -ggdb option produces debugging information for use by GDB
     """
-    subprocess.run(["gcc", "-ggdb", code_file])
+    subprocess.run(["gcc", "-ggdb", code])
 
 
-def debug_code():
+def debug_code(breakpoints):
     """
     Run GDB on the compiled executable
     Print out all stack args and locals at each frame where breakpoint is added
@@ -32,6 +46,8 @@ def debug_code():
     gdbmi.write("-file-exec-and-symbols a.out")
     # machine interface (MI) commands start with a '-'
     gdbmi.write("-break-insert main")
+    for breakpoint in breakpoints:
+        gdbmi.write(f"-break-insert {breakpoint}")
     # gdbmi.write('-break-insert append')
     # gdbmi.write('-enable-frame-filters')
     gdbmi.write("-exec-run")
