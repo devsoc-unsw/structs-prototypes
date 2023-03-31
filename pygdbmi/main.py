5488a5c8a8021ff06678fcdf3d8ec773c69c39d0
jedwed <83894732+jedwed@users.noreply.github.com>
2023-03-31 21:23:38 +1100

    Formatted code with black, added to requirements.txt

---
+++ pygdbmi/main.py
@@ -0,0 +1,71 @@
+import subprocess
+import sys
+from pygdbmi.gdbcontroller import GdbController
+
+# COMMAND_TIMEOUT_SEC = 0.1
+
+
+def main():
+    """
+    Main function
+    """
+    code_file = sys.argv[1]
+    compile_code(code_file)
+    debug_code()
+
+
+def compile_code(code_file):
+    """
+    Compiles code_file into executable
+    Note: -ggdb option produces debugging information for use by GDB
+    """
+    subprocess.run(["gcc", "-ggdb", code_file])
+
+
+def debug_code():
+    """
+    Run GDB on the compiled executable
+    Print out all stack args and locals at each frame where breakpoint is added
+    """
+    gdbmi = GdbController()
+
+    gdbmi.write("-file-exec-and-symbols a.out")
+    # machine interface (MI) commands start with a '-'
+    gdbmi.write("-break-insert main")
+    # gdbmi.write('-break-insert append')
+    # gdbmi.write('-enable-frame-filters')
+    gdbmi.write("-exec-run")
+
+    # Continue executing GDB commands until execution ends
+    while True:
+        # response = gdbmi.write('-exec-next', timeout_sec=COMMAND_TIMEOUT_SEC)[-1]['payload']
+        response = gdbmi.write("-exec-next")[-1]["payload"]
+        if "reason" in response and response["reason"] in [
+            "exited",
+            "exited-normally",
+            "exited-signalled",
+        ]:
+            break
+
+        frame_info = response["frame"]
+        # frame_variables = gdbmi.write('-stack-list-variables --simple-values', timeout_sec=COMMAND_TIMEOUT_SEC)[0]['payload']['variables']
+        frame_variables = gdbmi.write("-stack-list-variables --simple-values")[0][
+            "payload"
+        ]["variables"]
+        # frame_arguments = gdbmi.write('-stack-list-arguments --simple-values', timeout_sec=COMMAND_TIMEOUT_SEC)[0]['payload']['arguments']
+        # frame_locals = gdbmi.write('-stack-list-locals --simple-values', timeout_sec=COMMAND_TIMEOUT_SEC)[0]['payload']['locals']
+
+        print(f"Executing line {frame_info['line']} in function {frame_info['func']}")
+
+        for frame_variable in frame_variables:
+            if not all(k in frame_variable for k in ("name", "value")):
+                continue
+
+            if "arg" in frame_variable and frame_variable["arg"] == "1":
+                print(f"arg: {frame_variable['name']} = {frame_variable['value']}")
+            else:
+                print(f"local: {frame_variable['name']} = {frame_variable['value']}")
+
+
+if __name__ == "__main__":
+    main()
