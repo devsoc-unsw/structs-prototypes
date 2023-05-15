10e2de8b55c17d0ba609c54e025ba19265cdb2ab
david zhou <david@h2xengineering.com>
2023-05-15 20:00:38 +1000

    add more button, transition stub implementation to system

--- frontend/src/component/state/arrayDsParser.ts
+++ frontend/src/component/state/arrayDsParser.ts
@@ -1,14 +1,20 @@
 import { ArrayNode, ArrayDataStructure, SupportDataType, State } from './state'; // Replace with the actual file name containing your types
 
-function generateRandomArrayData(minValue: number, maxValue: number, length: number): ArrayDataStructure {
-  const getRandomValue = (min: number, max: number): number => Math.floor(Math.random() * (max - min + 1)) + min;
-  const getAddress = (index: number): string => `0x${(1000000 + index * 4).toString(16)}`;
+function generateRandomArrayData(
+  minValue: number,
+  maxValue: number,
+  length: number,
+): ArrayDataStructure {
+  const getRandomValue = (min: number, max: number): number =>
+    Math.floor(Math.random() * (max - min + 1)) + min;
+  const getAddress = (index: number): string =>
+    `0x${(1000000 + index * 4).toString(16)}`;
 
   const data: ArrayNode[] = [];
 
   for (let i = 0; i < length; i++) {
     data.push({
-      addr: { value: `arrayElement${i}`, addr: getAddress(i) },
+      addr: getAddress(i),
       data: getRandomValue(minValue, maxValue).toString(),
     });
   }
@@ -18,7 +24,6 @@ function generateRandomArrayData(minValue: number, maxValue: number, length: num
     data,
   };
 }
-
 
 /**
  * Randomly generate an array data structure
@@ -30,10 +35,13 @@ export function parseArrayState(): ArrayDataStructure {
 export function generateArrayState(): State {
   let ds = parseArrayState();
   return {
-    variables: [{
-      name: 'curr',
-      addr: ds.data[0].addr,
-    }],
+    variables: [
+      {
+        name: 'curr',
+        addr: ds.data[0].addr,
+        value: ds.data[0].data.toString(),
+      },
+    ],
     dataStructure: parseArrayState(),
   };
 }
